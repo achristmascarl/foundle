6f9d04013d03344567a12ed53600fc3117f50bf2
chrischerian <ccherian@wharton.upenn.edu>
2022-10-24 16:19:41 -0400

    Updated utils - Company List
    - Added LinkedIn
    - Added AirBnB
    - Added fundingAmount var
    - Added ipo var

--- src/utils.js
+++ src/utils.js
@@ -19,6 +19,8 @@ export const exampleData = [
       "First product: Apple I",
     ],
     foundingYear: 1976,
+    fundingAmount: 250000, //https://en.wikipedia.org/wiki/Apple_Inc
+    ipo: true,
   },
   {
     name: "Google",
@@ -32,5 +34,37 @@ export const exampleData = [
       "First product: Google Search",
     ],
     foundingYear: 1998,
+    fundingAmount: 36100000, //https://www.crunchbase.com/organization/google
+    ipo: true,
+  },
+  {
+    name: "LinkedIn",
+    slideUrls: [
+      //Insert Slide Here
+    ],
+    logoUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/LinkedIn_Logo.svg/291px-LinkedIn_Logo.svg.png?20170711102837",
+    facts: [
+      "Founded in 2003",
+      "Reid Hoffman",
+      "First product: LinkedIn Connections Database",
+    ],
+    foundingYear: 1998,
+    fundingAmount: 154800000, //https://www.crunchbase.com/organization/linkedin
+    ipo: false,
+  },
+  {
+    name: "AirBnB",
+    slideUrls: [
+      //Insert Slide Here
+    ],
+    logoUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Airbnb_Logo_B%C3%A9lo.svg/640px-Airbnb_Logo_B%C3%A9lo.svg.png",
+    facts: [
+      "Founded in 2008",
+      "Brian Chesky, Nathan Blecharczyk, and Joe Gebbia",
+      "First product: Airbnb",
+    ],
+    foundingYear: 2008,
+    fundingAmount: 6400000000, //https://www.crunchbase.com/organization/airbnb
+    ipo: true
   },
 ]
